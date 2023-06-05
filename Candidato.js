f4e8636a61c577036d84c35c5176ecc0717d5985
eduardobpo <eduardo.bispo.dias@escola.pr.gov.br>
2023-06-04 23:34:06 -0300

    Adicionad um array de objetos Candidato

--- Candidato.js
+++ Candidato.js
@@ -1,8 +1,16 @@
 class Candidato{
-	constructor(nome, nota){
+	constructor(id, nome, nota){
+		this.id = id;
 		this.nome = nome;
 		this.nota = nota;
 	}
 }
 
-var nota_corte = 6;
+const NOTA_CORTE = 6;
+
+var candidatos =[];
+candidatos.push(new Candidato(001, 'Fulano', 8));
+candidatos.push(new Candidato(002, 'Beltrano', 7.5));
+candidatos.push(new Candidato(003, 'Ciclano', 5.9));
+
+
